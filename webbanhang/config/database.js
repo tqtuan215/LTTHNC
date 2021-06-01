0d64e5af3a673c290339636991364ea94c7fb27a
tqtuan215 <tqtuan215@gmail.com>
2021-06-01 10:09:50 +0700

    btl
    btl

---
+++ webbanhang/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    'url' : 'mongodb+srv://tuan:%40tuan215@shopping.geb0l.mongodb.net/BanHang?retryWrites=true&w=majority'
+};
---
+++ webbanhang/public/js/tiente.js
@@ -0,0 +1,5 @@
+function formatCash(str) {
+    return str.split('').reverse().reduce((prev, next, index) => {
+        return ((index % 3) ? next : (next + ',')) + prev
+    })
+}
